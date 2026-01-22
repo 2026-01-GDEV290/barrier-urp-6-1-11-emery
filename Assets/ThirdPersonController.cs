5d5356feb79b3bd5245046a8fa9bca392d4bdb9b
FourRVCC30 <179720166+4RVCC30@users.noreply.github.com>
2026-01-22 15:40:29 -0500

    fixed the player movement and changed some anims

--- Assets/ThirdPersonController.cs
+++ Assets/ThirdPersonController.cs
@@ -12,20 +12,37 @@ public class ThirdPersonController : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    public GameObject trilRenderer;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     private void Update()
     {
         float Horizontal = Input.GetAxisRaw("Horizontal");
         float Vertical = Input.GetAxisRaw("Vertical");
         Vector3 dir = new Vector3(Horizontal, 0f, Vertical).normalized;
+        AnimatorStateInfo animInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (animInfo.IsName("Idle"))
+        {
+            trilRenderer.SetActive(false);
+        }
+        else if(animInfo.IsName("ATK"))
+        {
+            trilRenderer.SetActive(true);
+        }
 
         if (dir.magnitude >= 0.1f)
         {
             anim.SetBool("Walking", true);
-            float targetAngle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            controller.Move(dir * speed * Time.deltaTime);
+
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
         else
         {
